d64634e9798f0920ca2ca9517658914a23ce27e1
boza2000 <boza2000@abv.bg>
2026-02-06 16:39:38 +0200

    Extra Comment added
    To test Fork

--- src/mitsubishi2mqtt/mitsubishi2mqtt.ino
+++ src/mitsubishi2mqtt/mitsubishi2mqtt.ino
@@ -13,7 +13,7 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
+// Add Extra Line for Git
 #include "FS.h"               // SPIFFS for store config
 #ifdef ESP32
 #include <WiFi.h>             // WIFI for ESP32
